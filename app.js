2c4481315204788a065d8864b5b0e8091905491c
teste <maxwel.batalha@gmail.com>
2024-01-12 22:43:23 -0300

    pages

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
+const session = require('express-session');
+const route = require('./routes/route');
+
+
 const app = express();
-const routes = require('./routes/route')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views/pages');
 
-app.use('/', routes)
+app.use(session({secret: '123'}));
 
-app.listen(3000, () => {
-  console.log("Servidor ativo http://localhost:3000");
+app.use('/', route);
+app.use('/login', route);
+app.use('/singup', route);
+app.use('/logout', route);
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,96 @@
 const express = require('express');
-const { helloWorldController } = require('../controller/helloWorldController.js');
-
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user'); 
 
-router.get('/', helloWorldController);
+// Rota para renderizar a home
+router.get('/', async (req, res) => {
+    // Verifica se há um usuário na sessão
+    if (req.session.user) {
+        // Busca o usuário no banco de dados usando o e-mail armazenado na sessão
+        const user = await User.findOne({ where: { email: req.session.user.email } });
+        // Renderiza a página 'index' com os detalhes do usuário
+        res.render('index', { user });
+    } else {
+        // Se não houver usuário na sessão, renderiza a página de login sem mensagens de erro
+        res.render('login', { message: null });
+    }
+});
+
+// Rota para processar o formulário de login
+router.post('/', async (req, res) => {
+    // Obtém o e-mail e senha do corpo da requisição
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Busca o usuário no banco de dados usando o e-mail fornecido
+    const user = await User.findOne({ where: { email: email } });
+
+    // Verifica se o usuário existe e se a senha está correta
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        // Se o usuário não existir ou a senha estiver incorreta, renderiza a página de login com mensagem de erro
+        return res.render('login', { message: 'Email e/ou senha incorreta', value: email || '' });
+    }
+
+    // Salva o usuário na sessão
+    req.session.user = user;
+    
+    // Redireciona para a página inicial após o login
+    res.redirect('/');
+});
+
+// Rota para renderizar a página de cadastro
+router.get('/singup', (req, res) => {
+    res.render('singup', { message: null, value: null });
+    // Destrói a sessão
+    req.session.destroy();
+});
+
+// Rota para processar o formulário de cadastro
+router.post('/signup', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        // Verifica se o e-mail já está cadastrado
+        const existingUser = await User.findOne({ where: { email: email } });
+
+        if (existingUser) {
+            // Se o e-mail já estiver cadastrado, renderiza a página de cadastro com uma mensagem de erro
+            let value = {
+                name: name,
+                email: email,
+            };
+            // Destrói a sessão
+            req.session.destroy();
+            return res.render('singup', { message: 'Email já cadastrado.', value: value });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Cria um novo usuário
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        // Salva o novo usuário no banco de dados
+        await newUser.save();
+
+        // Renderiza a página de login com uma mensagem indicando que o cadastro foi realizado com sucesso
+        res.render('login', {
+            message: 'Cadastro realizado com sucesso. Faça login para continuar.', value: email});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro interno do servidor');
+    }
+});
+
+// Rota para realizar o logout
+router.get('/logout', (req, res) => {
+    // Destroi a sessão e redireciona para a página de login
+    req.session.destroy();
+    res.redirect('/login');
+});
 
 module.exports = router;
